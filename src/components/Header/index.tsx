1b0663c2bba30707715d18b389dc28b9c1609f7c
igomedeiros <igopdm@gmail.com>
2021-05-28 10:50:23 -0300

    Add modal component and create function like prop

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,20 @@
+import { useState } from 'react'
 import logoImg from '../../assets/dtmoney.svg'
 import { Container, Content } from './style'
 
-export function Header() {
+interface HeaderProps {
+    onOpenNewTransactionModal: () => void
+}
+
+export function Header(props: HeaderProps) {
+
+    const { onOpenNewTransactionModal } = props
+
     return (
         <Container>
             <Content>
                 <img src={logoImg} alt="dt money" />
-                <button type="button">Nova transação</button>
+                <button type="button" onClick={onOpenNewTransactionModal}>Nova transação</button>
             </Content>
         </Container>
     )
