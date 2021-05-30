f6cf90cf1ff0c006c5702390ac2940ddbe90c58f
igomedeiros <igopdm@gmail.com>
2021-05-30 09:11:29 -0300

    Using context to update TransactionTable

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -4,7 +4,6 @@ import closeImg from '../../assets/close.svg'
 import incomeImg from '../../assets/income.svg'
 import outcomeImg from '../../assets/outcome.svg'
 import { FormEvent, useState, useContext } from 'react'
-import { api } from '../../services/api'
 import { TransactionsContext } from '../../TransactionsContext'
 
 Modal.setAppElement('#root')
@@ -23,15 +22,21 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
     const [amount, setAmount] = useState(0)
     const [category, setCategory] = useState('')
 
-    function handleCreateNewTransaction(event: FormEvent) {
+    async function handleCreateNewTransaction(event: FormEvent) {
         event.preventDefault()
 
-        createTransaction({
+        await createTransaction({
             amount,
             category,
             title,
             type
         })
+
+        setType('deposit')
+        setTitle('')
+        setAmount(0)
+        setCategory('')
+        onRequestClose()
     }
 
     return (
