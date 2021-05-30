2262bcfc07597ca9bd654082e38639d7d2991be0
igomedeiros <igopdm@gmail.com>
2021-05-30 06:48:55 -0300

    Created miragejs schema to api

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -5,6 +5,7 @@ import incomeImg from '../../assets/income.svg'
 import outcomeImg from '../../assets/outcome.svg'
 import { FormEvent, useState } from 'react'
 import { title } from 'process'
+import { api } from '../../services/api'
 
 Modal.setAppElement('#root')
 
@@ -23,7 +24,14 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
     function handleCreateNewTransaction(event: FormEvent) {
         event.preventDefault()
 
+        const data = {
+            title,
+            value,
+            category,
+            type,
+        }
 
+        api.post('transactions', data)
     }
 
     return (
